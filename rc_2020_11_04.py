8f166399e27063fd21524ccfc10fe11524ab1043
Andrius Cepaitis <andrius@cepaitis.eu>
2020-11-05 07:01:16 +0200

    Patikrinti ka atspausdina su ivairiais pradiniais duomenimis

--- rc_2020_11_04.py
+++ rc_2020_11_04.py
@@ -16,6 +16,21 @@ def pastumti_zodi (a, k):
         i+=1
     return s
 
-print (pastumti_zodi("Rugile",6))
+import string
 
+az = string.ascii_lowercase
+AZ = string.ascii_uppercase
+m = [
+    ("Rugile", 6),
+    ("Rugile", 1),
+    ("Rugile", 0),
+    (az, 0),
+    (az, 1),
+    (az, 25),
+    (AZ, 0),
+    (AZ, 1),
+    (AZ, 25),
+]
 
+for x in m:
+    print (pastumti_zodi(*x), x)
